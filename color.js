e1b840e3dc98fc39a0d91855715648ad318db5ed
precisememory <tetracontakaidigon42@gmail.com>
2014-04-19 10:37:57 -0400

    Working on File modal dialogs
    Added some scaffolding for dialogs, need to work more on receiving file and doing something with it

--- color.js
+++ color.js
@@ -3,15 +3,48 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
   
+  $('#file-add-btn').click(fileAdd);
+  $('#file-add-btn2').click(handleFiles);
+  $('#manual-add-btn').click(manualAdd);
+  
+  
+  $('#more-info-btn').click(function(){
+	$('#storage').modal('hide');
+	$('#more-info').modal('show');
+  });
+  
   if(!checkForStorage()){
 	$('#storage').modal('show');
   } else if(!checkForDragons()){
 	$('#firstrun').modal('show');
   } else { //load dragons
-  
+	
   }
   
 });
+
+function handleFiles() {
+	var files = $('#file-input').files; //TODO: is this right?
+	if(!files){
+		$('#file-input-label').html("You must enter a file!");
+		//$('#file-add').modal('show');
+		return;
+	}
+	$('#file-add').modal('hide');
+    var file = files[0];
+	var objectURL = window.URL.createObjectURL(file);
+	//TODO: read in files line by line, assign each line to a dragon var
+} 
+
+function fileAdd(){
+	$('#firstrun').modal('hide');
+	$('#file-add').modal('show');
+}
+
+function manualAdd(){
+	$('#firstrun').modal('hide');
+	$('#manual-add').modal('show');
+}
 
 function checkForStorage(){
 	if(localStorage)
